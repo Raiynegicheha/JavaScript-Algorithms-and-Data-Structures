02e156440948698a0d8388a432db850ad0c4837f
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-17 02:52:44 +0300

    Update findelement.js
    Using the find() method.

--- findelement.js
+++ findelement.js
@@ -1,1 +1,11 @@
+// There are two methods that i used to solve this task.
+//the find() method.
+// the for loop
 
+function findElement(arr, func) {
+  return arr.find(func);
+  let num = 0;
+  return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
