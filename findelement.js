8a939112af1582d1c17b5f624157208696051dde
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-17 02:57:30 +0300

    Update findelement.js
    Using the for loop to solve task

--- findelement.js
+++ findelement.js
@@ -2,10 +2,26 @@
 //the find() method.
 // the for loop
 
+//using the find()
 function findElement(arr, func) {
   return arr.find(func);
   let num = 0;
   return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+
+//using the for loop
+function findElement(arr, func) {
+  let num = 0;
+
+  for(let i = 0; i < arr.length; i++){
+    num = arr[i];
+    if(func(num)){
+      return num;
+    }
+  }
 }
 
 findElement([1, 2, 3, 4], num => num % 2 === 0);
